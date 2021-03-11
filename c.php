36b9149e2ef00697875a510d2309df7d2350234c
crisstian <cdgranados19@gmail.com>
2021-03-11 01:58:07 -0500

    updated

---
+++ c.php
@@ -0,0 +1,24 @@
+<?php
+
+$conecta = mysqli_connect ('localhost', 'root', '', 'mascota') or die (mysql_error($mysqli));
+
+insertar($conecta);
+
+
+function insertar ($conecta){
+
+    $id = $_POST['id'];
+    $nombre = $_POST['nombre'];
+    $edad = $_POST['edad'];
+    $nombrecientifico = $_POST['nombrecientifico'];
+
+
+    $consulta = "INSERT TO INTO animales(nombre, edad, nombrecientifico)
+    VALUES  ( '$id','$nombre', '$edad', '$nombrecientifico')";
+
+    mysqli_query($conecta, $consulta);
+    mysqli_close($conecta);
+
+}
+
+?>
